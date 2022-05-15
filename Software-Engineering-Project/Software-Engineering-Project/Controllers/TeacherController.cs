7818e74a1cea215b2388a043f78e0c5ee050d561
marata13 <ioakeim13@hotmail.gr>
2022-05-16 00:10:03 +0300

    fixed some bugs

--- Software-Engineering-Project/Software-Engineering-Project/Controllers/TeacherController.cs
+++ Software-Engineering-Project/Software-Engineering-Project/Controllers/TeacherController.cs
@@ -35,9 +35,9 @@ namespace Software_Engineering_Project.Controllers
                 NpgsqlConnection conn = Database.Database.GetConnection();
                 int result = Database.Database.ExecuteUpdate(String.Format("insert into users (username , password,first_name,last_name" +
                     ",gender,email,phone,role) values ('{0}','{1}','{2}','{3}','{4}','{5}',{6},'{7}');" +
-                    " insert into student (student , start_year) values ('{8}', {9})",
+                    " insert into student (student , start_year, professor) values ('{8}', {9}, {10})",
                     model.Username, model.Password, model.FirstName, model.LastName, model.Gender, model.Email, model.Phone,
-                    model.Role, model.Username, model.StartYear), conn);
+                    model.Role, model.Username, model.StartYear, model.Professor), conn);
                 if (result != 0)
                 {
                     conn.Close();
@@ -49,6 +49,26 @@ namespace Software_Engineering_Project.Controllers
             ViewBag.Success = false;
             return View();
 
+        }
+
+        public IActionResult Search(string queryString)
+        {
+            if(queryString.Contains(' '))
+            {
+                string firstName = queryString.Substring(0, queryString.IndexOf(' '));
+                string lastName = queryString.Substring(queryString.IndexOf(' ') + 1);
+
+                NpgsqlConnection conn = Database.Database.GetConnection();
+                NpgsqlDataReader reader = Database.Database.ExecuteQuery(String.Format("select student" +
+                    ", start_year, first_name, last_name, email, phone, title, thesis_start_date, " +
+                    "grade, language, technology from thesis natural join (select student, start_year," +
+                    " first_name, last_name, email, phone from student as s join users as u on " +
+                    "s.student = u.username where first_name='{0}' and last_name='{1}' " +
+                    "and professor='{2}') as student",firstName,lastName), conn);
+
+
+            }
+            return View();
         }
     }
 }
