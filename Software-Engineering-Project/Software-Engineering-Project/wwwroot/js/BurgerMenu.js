256a2245312f66cde185245f7d245ff11f776bd6
p19140 <marianapetropoulou@gmail.com>
2022-05-26 22:22:25 +0300

    burger menu - in teacher home page

---
+++ Software-Engineering-Project/Software-Engineering-Project/wwwroot/js/BurgerMenu.js
@@ -0,0 +1,12 @@
+﻿function myFunction() {
+
+
+
+    let x = document.getElementById("bmenu");
+    if (x.className === "menu") {
+        x.className += " responsive";
+
+    } else {
+        x.className = "menu";
+    }
+}
