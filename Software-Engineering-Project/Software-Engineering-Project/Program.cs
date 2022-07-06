9dd9d80a4b15425a037822b061414cf7f10fb553
aboublini <p19117@unipi.gr>
2022-07-06 15:31:48 +0300

    teacher profile test

--- Software-Engineering-Project/Software-Engineering-Project/Program.cs
+++ Software-Engineering-Project/Software-Engineering-Project/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Login}/{id?}");
+    pattern: "{controller=Teacher}/{action=Profile}/{id?}");
 
 app.Run();
